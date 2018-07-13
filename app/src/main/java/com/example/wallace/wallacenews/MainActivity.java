60ba10cd5b1a16f18850e47a9df71d6e8dd79b9b
miseryby <13750669660@163.com>
2018-07-13 20:20:11 +0800

    recyview almost done

--- app/src/main/java/com/example/wallace/wallacenews/MainActivity.java
+++ app/src/main/java/com/example/wallace/wallacenews/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.wallace.wallacenews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioButton mRadioButton1,mRadioButton2,mRadioButton3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_content );
+        mRadioButton1 =(RadioButton) findViewById( R.id.btn1 );
     }
 }
