ac3930b8dc8495536975f54c8ebf46e9ca97ef80
PengHanSheng <1281153804@qq.com>
2018-07-15 20:20:47 +0800

    获取网络新闻工具类和新闻的data类

--- app/src/main/java/com/example/wallace/wallacenews/yang/ui/NewsListFragment.java
+++ app/src/main/java/com/example/wallace/wallacenews/yang/ui/NewsListFragment.java
@@ -1,6 +1,5 @@
 package com.example.wallace.wallacenews.yang.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
