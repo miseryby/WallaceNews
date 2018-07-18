01867569fa4a8a74990e6bf7e73050a0b13dae39
1412716744 <1412716744@qq.com>
2018-07-18 08:58:18 +0800

    我的界面

--- app/src/main/java/com/example/wallace/wallacenews/yang/ui/MeActivity.java
+++ app/src/main/java/com/example/wallace/wallacenews/yang/ui/MeActivity.java
@@ -1,10 +1,21 @@
 package com.example.wallace.wallacenews.yang.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.wallace.wallacenews.R;
 
 public class MeActivity extends  SingleFragmentActivity {
+
+
     @Override
     protected Fragment creatFragment() {
         return new NewsMeFragment();
     }
+
+
 }
